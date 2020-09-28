ba432da80cc5283856843065929b87f34cd9301a
luck_nut <hasyrawi@gmail.com>
2020-09-29 04:23:30 +0800

    extract skills to multiple skills

--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -4,7 +4,16 @@ import photoProfile from './component/hanan.jpg'
 import { Skill, Hero } from './component';
 
 class Homepage extends Component {
+
   render() {
+
+    const skills = [
+      { id: 1, name: 'JavaScript', logo: "https://img.icons8.com/clouds/100/000000/github.png" },
+      { id: 2, name: 'Python', logo: "https://img.icons8.com/dusk/128/000000/python.png" },
+      { id: 3, name: 'PHP', logo: "https://img.icons8.com/dusk/128/000000/php-logo.png" },
+      { id: 4, name: 'DART', logo: "https://img.icons8.com/color/100/000000/dart.png" }
+    ]
+
     return (
       <div className="homepage">
         <Navbar />
@@ -16,7 +25,19 @@ class Homepage extends Component {
             pengalaman berselancar di dunia pemograman sangat lah saya gemari sejak
             duduk di bangku smp karenanya saya dan pemograman ialah jalan ninja ku 😎
         </Hero>
-        <Skill>
+        <Skill
+          title="Alat Tempur"
+          skills={skills}
+        >
+          Tentu saja seorang developer takluput dengan bahasa yang di gunakan sebagai
+          alat tempur dalam menyelesaikan suatu problem tertentu . bahasa yang saat ini
+          yang saya percayai dalam menyelesaikan masalah ialah
+        </Skill>
+
+        <Skill
+          title="Framework"
+          skills={skills}
+        >
           Tentu saja seorang developer takluput dengan bahasa yang di gunakan sebagai
           alat tempur dalam menyelesaikan suatu problem tertentu . bahasa yang saat ini
           yang saya percayai dalam menyelesaikan masalah ialah
--- src/pages/home/component/Skill.jsx
+++ src/pages/home/component/Skill.jsx
@@ -1,26 +1,19 @@
 import React from 'react'
 import Card from './Card';
 
-const Skill = ({ children, ...props }) => {
 
-  const skills = [
-    { id: 1, name: 'JavaScript', logo: "https://img.icons8.com/clouds/100/000000/github.png" },
-    { id: 2, name: 'Python', logo: "https://img.icons8.com/dusk/128/000000/python.png" },
-    { id: 3, name: 'PHP', logo: "https://img.icons8.com/dusk/128/000000/php-logo.png" },
-    { id: 4, name: 'DART', logo: "https://img.icons8.com/color/100/000000/dart.png" }
-  ]
+const Skill = ({ children, title, skills }) => {
 
   const listOfSkill = skills.map((item, index) => {
     return (
       <Card key={item.id} title={item.name} logo={item.logo} />
     )
   })
-
   return (
     <section className="text-gray-500 bg-gray-900 body-font">
       <div className="container px-5 py-24 mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
-          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Alat Perang</h1>
+          <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">{title}</h1>
           <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
             {children}
           </p>
