b411353e68c487ef4b7c3eec03ac2228a37bb391
luck_nut <hasyrawi@gmail.com>
2020-09-29 23:16:23 +0800

    Refactor About Pages

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -2,60 +2,45 @@ import React from 'react';
 import { StepLayout } from './component';
 import StepItem from './component/StepItem';
 
-function About(props) {
+const About = () => {
+
+  const riwaytPendidikan = [
+    { id: 1, name: 'SDN 175 KAWARASAN', desc: "2004 - 2010" },
+    { id: 2, name: 'SMP 1 TOMONI', desc: "2010 - 2014" },
+    { id: 3, name: 'SMA MUHAMADIYAH WOTO', desc: "2014 - 2016" },
+    { id: 4, name: 'Universitas Muslim Indonesia', desc: "2017 - presents" },
+  ];
+  const riwayatPekerjaan = [
+    { id: 1, name: 'PickUpz Shipment Aggregator', desc: "2004 - 2010" },
+    { id: 2, name: 'GMT Building Management', desc: "2017 - presents" },
+  ];
+
+  const ListPendidikan = riwaytPendidikan.map((item, index) => (
+    <StepItem key={index}
+      title={item.name}
+      desc={item.desc}
+      steps={index + 1}
+      animation="fadeIn"
+      delay="100"
+    />
+  ));
+  const ListPekerjaan = riwayatPekerjaan.map((item, index) => (
+    <StepItem key={index}
+      title={item.name}
+      desc={item.desc}
+      steps={index + 1}
+      animation="fadeIn"
+      delay="100"
+    />
+  ));
+
   return (
     <div className="about">
       <StepLayout title="Riwayat Pendidikan">
-        <StepItem
-          animation="fadeIn"
-          delay="100"
-          title="SDN 175 KAWARASAN"
-          desc="2004 - 2010"
-        />
-        <StepItem
-          animation="fadeIn"
-          delay="150"
-          title="SMP 1 TOMONI"
-          desc="2010 - 2014"
-        />
-        <StepItem
-          animation="fadeIn"
-          delay="200"
-          title="SMA MUHAMADIYAH WOTO"
-          desc="2014 - 2016"
-        />
-        <StepItem
-          delay="220"
-          animation="fadeIn"
-          title="Universitas Muslim Indonesia"
-          desc="2017 - presents"
-        />
+        {ListPendidikan}
       </StepLayout>
-      <StepLayout title="Riwayat Cinta">
-        <StepItem
-          animation="fadeIn"
-          delay="100"
-          title="Tidak Ada"
-          desc="2004 - 2010"
-        />
-        <StepItem
-          animation="fadeIn"
-          delay="150"
-          title="Tidak Ada"
-          desc="2010 - 2014"
-        />
-        <StepItem
-          animation="fadeIn"
-          delay="200"
-          title="Tidak Ada"
-          desc="2014 - 2016"
-        />
-        <StepItem
-          delay="220"
-          animation="fadeIn"
-          title="Tidak Ada"
-          desc="2017 - presents"
-        />
+      <StepLayout title="Riwat Pekerjaan">
+        {ListPekerjaan}
       </StepLayout>
     </div>
   );
--- src/pages/about/component/StepItem.jsx
+++ src/pages/about/component/StepItem.jsx
@@ -3,7 +3,7 @@ import aos from 'aos'
 
 const StepItem = (props) => {
 
-  const { desc, animation, title , delay='200' } = props;
+  const { desc, animation, title, delay = '200', steps } = props;
 
   useEffect(() => {
     aos.init()
@@ -14,7 +14,7 @@ const StepItem = (props) => {
       <div className="h-full w-6 absolute inset-0 flex items-center justify-center">
         <div className="h-full w-1 bg-gray-800 pointer-events-none"></div>
       </div>
-      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">1</div>
+      <div className="flex-shrink-0 w-6 h-6 rounded-full mt-10 sm:mt-0 inline-flex items-center justify-center bg-indigo-500 text-white relative z-10 title-font font-medium text-sm">{steps}</div>
       <div className="flex-grow md:pl-8 pl-6 flex sm:items-center items-start flex-col sm:flex-row">
         <div className="flex-shrink-0 w-24 h-24 bg-gray-800 text-indigo-400 rounded-full inline-flex items-center justify-center">
           <svg data-aos={animation} data-aos-delay={delay} fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-12 h-12" viewBox="0 0 24 24">
