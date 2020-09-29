05128d67b3080e8c6356c34ac5580e20341dc7c0
luck_nut <hasyrawi@gmail.com>
2020-09-29 23:37:41 +0800

    refactor projects pages

---
+++ src/mocks/data.js
@@ -0,0 +1,26 @@
+const projectsData = [
+  {
+    id: Date.now(),
+    projectName: 'Commercial Management System',
+    title: 'Telkom',
+    desc: 'Commercil Asset Telkom dari Tingakt Daerah Sampe ke Tingkat Provinsi'
+  },
+
+  {
+    id: Date.now(),
+    projectName: 'Building Management System',
+    title: 'Telkom',
+    desc: 'Pusat Monitor Dari 3 App Web Yang Dibuat Diatas dengan Menghubungkan nya'
+  },
+  {
+    id: Date.now(),
+    projectName: 'Telkom Dashboard',
+    title: 'Telkom',
+    desc: 'Pusat Monitor Dari 3 App Web Yang Dibuat Diatas dengan Menghubungkan nya'
+  },
+
+]
+
+export {
+  projectsData,
+}
--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { CardProjects, HeaderProjects } from './component';
 
+import { projectsData } from '../../mocks/data'
+
 const Projects = () => {
+
+  const listOfProjects = projectsData.map(item => (
+    <CardProjects
+      projecName={item.projectName}
+      title={item.title}
+      desc={item.desc}
+    />
+  ))
+
   return (
     <section className="text-gray-500 bg-gray-900 body-font">
       <div className="container px-5 py-24 mx-auto">
         <HeaderProjects title="The Projects" />
         <div className="flex flex-wrap -m-4">
-          <CardProjects
-            projecName="Commercial Management System"
-            title="Telkom"
-            desc="Commercil Asset Telkom dari Tingakt Daerah Sampe ke Tingkat Provinsi"
-          />
-          <CardProjects
-            projecName="Building Management System"
-            title="Telkom"
-            desc="Building Management System Untuk Asset Telkom"
-          />
-          <CardProjects
-            projecName="Telkom Dashboard"
-            title="Telkom"
-            desc="Pusat Monitor Dari 3 App Web Yang Dibuat Diatas dengan Menghubungkan nya"
-          />
+          {listOfProjects}
         </div>
       </div>
     </section>
